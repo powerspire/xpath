1aaf3b1591a06c54cbaf498c8d4e851047a80526
Liam Power <lpower@gonitro.com>
2019-04-11 15:50:05 +0100

    Updates and improvements

--- scanner/scan1.py
+++ scanner/scan1.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function
+
+from time import sleep
+
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 import sys, getopt, ntpath
 
 reload(sys)
@@ -16,8 +20,9 @@ browser_drivers = {
 # webPage = "http://bbc.co.uk/"
 
 def main(argv):
-    inputfile = ""
-    outputfile = ""
+    inputfile = ''
+    outputfile = ''
+
     filename = ""
     browser_to_use = ""
 
@@ -44,40 +49,57 @@ def main(argv):
 
     browser_driver = browser_drivers[browser_to_use]()
     browser_driver.get("{}".format(inputfile))
+    sleep(10)
+    browser_driver.maximize_window()
+    # browser_driver.get(webPage)
 
-    attribute_list = [
-        ["input", "id", "name", "type", "placeholder"],
-        ["link", "id", "rel", "type", "class"],
+    # The element type and  list of the attributes to use to generate the xpath expression
+    # The attributes can be replaced or added to suit applications
+    element_attribute_list = [
+        ["link", "id", "rel", "href", "class"],
         ["a", "id", "text", "class"],
-        ["img", "id", "alt", "class"]
+        ["button", "id", "type", "class", "text"],
+        ["input", "id", "name", "type", "placeholder"],
+        ["img", "id", "alt", "class", "src"]
     ]
-    for a in attribute_list:
-        attribute = "//" + a[0]
+    for elem_type in element_attribute_list:
+        attribute = "//" + elem_type[0]
+
+        # get a list of all elements of this type
         all_elements = browser_driver.find_elements_by_xpath(attribute)
         # print "Found total of {} <{}>  elements".format(len(allElements), a[0])
 
         el_count = 0
         for element in all_elements:
             el_count += 1
-            xpath = elemprint(a, element)
+            xpath = xpath_generate(elem_type, element)
 
             # Evaluate the xpath expression if it's not empty
             if xpath != "":
-                instance_count = len(browser_driver.find_elements_by_xpath(xpath))
+                try:
+                    instance_count = len(browser_driver.find_elements_by_xpath(xpath))
+                except NoSuchElementException:
+                    print ("!!! Bad expression -> {}", xpath)
                 print("Xpath expression {} found {} elements".format(xpath, instance_count))
+                if instance_count == 1:             # If one instance found, we already have a unique expression
+                    continue
 
-                # for instance in browser.find_elements_by_xpath(xpath):
-                # Look for nearest ancestor with an ID
+                # Look for nearest ancestor with an ID attribute
                 attrib = "@id"
                 ancestors = browser_driver.find_elements_by_xpath(xpath + "/ancestor::*[{}][1]".format(attrib))
                 if len(ancestors) == 0:
-                    attrib = "text()"
+                    # Look for nearest ancestor with a name attribute
+                    attrib = "@name"
                     ancestors = browser_driver.find_elements_by_xpath(xpath + "/ancestor::*[{}][1]".format(attrib))
+                    if len(ancestors) == 0:
+                        # Look for nearest ancestor with a text attribute
+                        attrib = "text()"
+                        ancestors = browser_driver.find_elements_by_xpath(xpath + "/ancestor::*[{}][1]".format(attrib))
 
                 for ancestor in ancestors:
-                    variable_name = "{}_{}_{}".format(ancestor.tag_name, ancestor.get_attribute("id"), a[0])
+                    variable_name = "{}_{}_{}".format(ancestor.tag_name, ancestor.get_attribute("id"), elem_type[0])
                     print(
-                        "Unique locator is {}".format(variable_name) + "=\"//{}[{}='{}']{}\""
+                        "...Unique locator is {}".format(variable_name) + "=\"//{}[{}='{}']{}\""
                         .format(ancestor.tag_name, attrib, ancestor.get_attribute("id"), xpath))
                     # If not unique yes, then need to go bak up more
 
@@ -85,15 +107,16 @@ def main(argv):
     fileout.close()
 
 
-# Print out the attributes of a element and their values
-def elemprint(a, e):
+# Generate an xpath expression based on the attributes of a element and their values
+def xpath_generate(a, e):
     # print ("#{}  <{}> ".format(el_count, a[0]).encode('utf-8', 'ignore'))
     elementtype = a[0]
     x_path_id = ""
-    a = a[1:]  # remove 1st element
+    a = a[1:]  # remove element type so only attribute remain
     attrib_count = 0
     for attrib in a:
-        attrib_value = e.get_attribute(attrib)
+        # attrib_value = e.get_attribute(attrib)
+        attrib_value = e.get_property(attrib)
         if attrib_value == "":
             # attrib_value = "NULL"
             continue  # ignore empty attributes
